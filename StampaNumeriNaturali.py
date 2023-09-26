ff40dafe453278f780026ea924f9e10ae2330b16
Pisano <salvatore.pisano@accenture.com>
2023-09-26 15:05:09 +0200

    modificato codice

--- StampaNumeriNaturali.py
+++ StampaNumeriNaturali.py
@@ -1,3 +1,11 @@
+valore1 = input("Inserisci il primo valore: ")
+valore2 = input("Inserisci il secondo valore: ")
 
-for i in range(1,11):
-    print(i);
+if valore1.isnumeric() and valore2.isnumeric():
+    risultato = int(valore1) * int(valore2)
+    print("Il risultato della moltiplicazione è:", risultato)
+elif valore1.isalpha() and valore2.isalpha():
+    risultato = valore1 + valore2
+    print("La concatenazione delle stringhe è:", risultato)
+else:
+    print("PESTO MISTO")
